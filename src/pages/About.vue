17ce0b65f6a1535767bd88cba747f82845fff3da
samuelokediji <samuelolaideokediji@gmail.com>
2020-05-23 01:25:05 +0100

    new

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,7 +3,7 @@
     <div class="about">
       <div class="about-text">
         <h1>About Me</h1>
-        <p>They don’t want us to eat. You see that bamboo behind me though, you see that bamboo? Ain’t nothin’ like bamboo. Bless up. Egg whites, turkey sausage, wheat toast, water. Of course they don’t want us to eat our breakfast, so we are going to enjoy our breakfast. Mogul talk. They will try to close the door on you, just open it. You smart, you loyal, you a genius. Special cloth alert. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. Another one. You see the hedges, how I got it shaped up? It’s important to shape up your hedges, it’s like getting a haircut, stay fresh.</p>
+        <p>Freshest Nigga in town. Oboy content hard. We just trying to climb the ladder wey no dey. Imaginary metamorphosing</p>
       </div>
       <div class="about-photo">
         <g-image src="~/assets/images/person.jpg" width="300" />
